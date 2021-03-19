c71abf540e12c0a78b6bae2267d975ff3e61fd5b
Lucas Ferreira da Silva <75485471+ferris77@users.noreply.github.com>
2021-03-18 23:43:59 -0300

    ajusted sliders

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
-<h1>Projeto de um WebApp que executa previsões baseado em um modelo de machine learning</h1>
-Este projeto consiste em um webapp que recebe dados de um usuário e retorna uma mensagem com o diagnóstico conforme previsão de um modelo de machine learning do tipo Random Forest Classifier. O modelo treinado foi serializado e carregado no site, de modo que sempre que os valores de entrada são alterados é feita a previsão.
-  
-\
-***Link do web app:http://ferris77.pythonanywhere.com/***
+<h1>Project of a Web App that performs predictions based on a machine learning model</h1>
+<h3>Description</h3>
+This project consists of a web app that receives data from a user and returns a message with the diagnosis according to the prediction of a machine learning model of the Random Forest Classifier type. The trained model was serialized and uploaded to the site. Whenever the input values are changed, the forecast is made.
+
+A Random Foresc Classifier model was created and tuned. The verall accuracy achieved was 84.42%
+
+<h3>Access</h3>
+Link to web app: ***http://ferris77.pythonanywhere.com/***
 
 <h3>Acknowledgements</h3>
-Smith, J.W., Everhart, J.E., Dickson, W.C., Knowler, W.C., & Johannes, R.S. (1988). Using the ADAP learning algorithm to forecast the onset of diabetes mellitus. In Proceedings of the Symposium on Computer Applications and Medical Care (pp. 261--265). IEEE Computer Society Press.  
-https://www.kaggle.com/uciml/pima-indians-diabetes-database
+1. Smith, J.W., Everhart, J.E., Dickson, W.C., Knowler, W.C., & Johannes, R.S. (1988). Using the ADAP learning algorithm to forecast the onset of diabetes mellitus. In Proceedings of the Symposium on Computer Applications and Medical Care (pp. 261--265). IEEE Computer Society Press.
+2. https://www.kaggle.com/uciml/pima-indians-diabetes-database
+
+<h3>About</h3>
+This is one of my first projects in my Machine Learning studies. Feedbacks and impressions are most welcome :)
--- app.py
+++ app.py
@@ -1,29 +1,25 @@
-<<<<<<< HEAD
-||||||| 440e2ba
-import flask
-import pandas as pd
-from flask import Flask, render_template, request, redirect, url_for
-=======
-import pandas as pd
-from flask import Flask, render_template, request
->>>>>>> development
 import joblib
-<<<<<<< HEAD
 import pandas as pd
 from flask import Flask, render_template, request
-||||||| 440e2ba
-import pickle
-=======
->>>>>>> development
 
+# creates a global Falsk instance sets the variable name to our module name ('app')
 app = Flask(__name__)
 
+# reload a persisted file we previously saves, ou Prediction Model in this case
 loaded_model = joblib.load('model.pkl')
 
+
+# use decorator to tell Flask witch URL should trigger our function
+# we are loading index.html with default values
 @app.route('/')
 def index():
+    """
+        function display our index.html template in the user's browser
+        Input str prediction_text -> is the default text displayed while waiting user input
+        Input int/float -> v1 - v8 are the input variables, they have been set to default values (middle scale)
+    """
     return render_template('index.html',
-                           prediction_text='Altere os valores papra gerar previsão',
+                           prediction_text='Change parameters to get prediction',
                            v1=3,
                            v2=90,
                            v3=90,
@@ -33,9 +29,19 @@ def index():
                            v7=0.500,
                            v8=50)
 
-@app.route('/predict', methods=['POST',])
-def make_prediction():
 
+# use decorator to tell Flask witch URL should trigger our function
+@app.route('/predict', methods=['POST', ])
+def make_prediction():
+    """
+        function to take user input data, feed the model and return prediction
+        we use a POST method to request data and save it to our variables
+        our prediction model returns '1' for positive diagnosis and '0' for negative
+        lastly, we render the html page with the current values, for better experience
+
+        Inputs -> n/a
+        Outputs str -> msg with diagnoses message
+    """
     if request.method == 'POST':
         pregnancies = request.form['pregnancies']
         glucose = request.form['glucose']
@@ -55,31 +61,27 @@ def make_prediction():
                      'DPF': DPF,
                      'age': age
                      }
-
+        # the model need a DataFrame as input, without  data indexes
         user_input = pd.DataFrame(user_data, index=[0])
         [prediction] = loaded_model.predict(user_input)
 
-<<<<<<< HEAD
     if prediction == 1:
-        msg = "A previsão é de diagnóstico positivo, tem diabetes."
-||||||| 440e2ba
-    if prediction = 1:
-        msg = "A previsão é de diagnóstico positivo, tem diabetes."
-=======
-    if prediction == 1:
-        msg = "A previsão é de que o paciente tem diabetes."
->>>>>>> development
+        msg = "The prediction is positive diagnosis, patient has diabetes."
     else:
-        msg = "A previsão é de que o paciente não tem diabetes."
+        msg = "The prediction is negative diagnosis, patient doesn't have diabetes."
 
-    return render_template("index.html",   prediction_text=msg,
-                                           v1=pregnancies,
-                                           v2=glucose,
-                                           v3=blood_pressure,
-                                           v4=skin_thickness,
-                                           v5=insulin,
-                                           v6=BMI,
-                                           v7=DPF,
-                                           v8=age)
+    # reloads the page, but keep the current values, this way user can keep interacting with parameters
+    return render_template("index.html", prediction_text=msg,
+                           v1=pregnancies,
+                           v2=glucose,
+                           v3=blood_pressure,
+                           v4=skin_thickness,
+                           v5=insulin,
+                           v6=BMI,
+                           v7=DPF,
+                           v8=age)
+
+
+# starts the server
 if __name__ == '__main__':
     app.run(debug=True)
